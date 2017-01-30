5170b7281ea3c84c8a99b270c8574f5e341811d8
Matteo Ferrando <matteo.ferrando2@gmail.com>
2017-01-29 22:11:55 -0400

    added the hangman software, missing some styling

--- hangman.js
+++ hangman.js
@@ -1,15 +1,29 @@
-const SIZE = 11;
+const CREOENTODOS = "CREOENTODOS";
+const SIZE = _.size(CREOENTODOS);
 const LETTERS ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-function tryRandomLetter(ind) {
-  _.random
+function clearRow() {
+  _.times(SIZE, (i) => {
+    id$(`H-${i}`).text(' ');
+  });
 }
 
 function cycleLetter(ind) {
-  const finding = id$(`H-${ind}`);
-  tryRandomLetter(ind);
+  if (ind == 0) {
+    clearRow();
+  }
+
+  setTimeout(function() {
+    const toFind = CREOENTODOS[ind];
+    const letter = _.sample(LETTERS);
+    const nextInd = (toFind == letter ? ind + 1 : ind) % SIZE;
+
+    id$(`H-${ind}`).text(letter);
+
+    cycleLetter(nextInd);
+  }, 100);
 }
 
 $(document).ready(function() {
-  
+  cycleLetter(0);
 });
