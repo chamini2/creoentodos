de2bc80b2abd71d343523aceaa22e02000383ad9
Matteo Ferrando <matteo.ferrando2@gmail.com>
2017-01-30 00:50:53 -0400

    underline for selected letter in hangman

--- hangman.js
+++ hangman.js
@@ -2,28 +2,42 @@ const CREOENTODOS = "CREOENTODOS";
 const SIZE = _.size(CREOENTODOS);
 const LETTERS ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+function Hid$(id) {
+  return id$(`H-${id}`);
+}
 function clearRow() {
   _.times(SIZE, (i) => {
-    id$(`H-${i}`).text(' ');
+    Hid$(i).text('_');
   });
 }
 
+function setUnderline(ind) {
+  clss$('choosing').removeClass('choosing');
+  Hid$(ind).addClass('choosing');
+}
+
 function cycleLetter(ind) {
-  if (ind == 0) {
-    clearRow();
+  if (ind == 11) {
+    // restart the whole thing
+    setTimeout(function() {
+      clearRow();
+      cycleLetter(0)
+    }, 500);
+  } else {
+    setUnderline(ind);
+    setTimeout(function() {
+      const toFind = CREOENTODOS[ind];
+      const letter = _.sample(LETTERS);
+      const nextInd = toFind == letter ? ind + 1 : ind;
+
+      Hid$(ind).text(letter);
+
+      cycleLetter(nextInd);
+    }, 100);
   }
-
-  setTimeout(function() {
-    const toFind = CREOENTODOS[ind];
-    const letter = _.sample(LETTERS);
-    const nextInd = (toFind == letter ? ind + 1 : ind) % SIZE;
-
-    id$(`H-${ind}`).text(letter);
-
-    cycleLetter(nextInd);
-  }, 100);
 }
 
 $(document).ready(function() {
+  clearRow();
   cycleLetter(0);
 });
